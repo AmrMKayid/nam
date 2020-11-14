807cc340c22115e9963a523c4ba75e2472ac7d3c
AmrMKayid <amrmkayid@gmail.com>
2020-11-14 17:54:55 +0200

    NAM types

---
+++ nam/types.py
@@ -0,0 +1,10 @@
+from typing import Sequence
+from typing import Tuple
+from typing import Union
+
+import numpy as np
+
+from nam.config.base import Config
+
+Config = Config
+DataType = Tuple[np.array, np.array]
