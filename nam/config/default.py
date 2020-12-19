adfe5e77bd63816bf9cadeb1fa993816045ed6d3
AmrMKayid <amrmkayid@gmail.com>
2020-12-19 04:58:02 +0200

    Update default configurations

--- nam/config/default.py
+++ nam/config/default.py
@@ -6,31 +6,36 @@ from nam.config.base import Config
 def defaults() -> Config:
   config = Config(
       device='cuda' if torch.cuda.is_available() else 'cpu',
-      logdir="logs",
+      output_dir="output",
+      training_epochs=10,
       lr=1e-2,
       batch_size=1024,
-      l2_regularization=0.0,
-      output_regularization=0.0,
-      decay_rate=0.995,
+
+      ## regularization_techniques
       dropout=0.5,
       feature_dropout=0.0,
-      data_split=1,
-      seed=1377,
+      decay_rate=0.995,
+      l2_regularization=0.0,
+      output_regularization=0.0,
+      ## Num units for FeatureNN
       num_basis_functions=1000,
       units_multiplier=2,
+      num_units=64,  ## TODO: number of hidden units? make it a list?!
+      data_split=1,
+      seed=1377,
       cross_val=False,
-      max_checkpoints_to_keep=1,  # TODO: checkpointer
-      save_checkpoint_every_n_epochs=10,
       n_models=1,
       num_splits=3,
       fold_num=1,
       activation='exu',
+      shuffle=True,
       regression=False,
       debug=False,
       shallow=False,
       use_dnn=False,
-      early_stopping_epochs=60,
+      patience=10,  ## For early stopping
       n_folds=5,
+      num_workers=16,  ## for dataloaders
   )
 
   return config
