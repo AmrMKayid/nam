5df837b141a9023120ed083e531b576bd0350811
AmrMKayid <amrmkayid@gmail.com>
2020-12-19 03:30:17 +0200

    Create `get_num_units` method to calculate units for NAM model

--- nam/models/__init__.py
+++ nam/models/__init__.py
@@ -2,3 +2,4 @@ from .base import Model
 from .dnn import DNN
 from .featurenn import FeatureNN
 from .nam import NAM
+from .utils import get_num_units
---
+++ nam/models/utils.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+from typing import List
+import torch
+
+
+def get_num_units(
+    config,
+    train_dataloader: torch.utils.data.DataLoader,
+) -> List:
+  features, _ = next(iter(train_dataloader))
+
+  num_unique_vals = [
+      len(np.unique(features[:, i])) for i in range(features.shape[1])
+  ]
+
+  num_units = [
+      min(config.num_basis_functions, i * config.units_multiplier)
+      for i in num_unique_vals
+  ]
+
+  return num_units
