9483a3da883003906c75664ddb0b568aa15d598b
AmrMKayid <amrmkayid@gmail.com>
2021-02-13 16:33:13 +0200

    Update FeatureNN
    --
    * First layer is `ExU` unit
    * Followed by regular `LinRelu` units

--- nam/models/featurenn.py
+++ nam/models/featurenn.py
@@ -4,7 +4,8 @@ import torch.nn.functional as F
 
 from nam.models.base import Model
 
-from .activation import ExU, LinReLU
+from .activation import ExU
+from .activation import LinReLU
 
 
 class FeatureNN(Model):
@@ -30,22 +31,22 @@ class FeatureNN(Model):
     self._input_shape = input_shape
     self._num_units = num_units
     self._feature_num = feature_num
-    self._activation = self.config.activation
 
-    hidden_sizes = [self._input_shape, self._num_units
-                   ] + self.config.hidden_sizes + [1]
+    hidden_sizes = [self._num_units] + self.config.hidden_sizes + [1]
 
     layers = []
-    layer = ExU if self.config.activation.lower() == 'exu' else LinReLU
+
+    ## First layer is ExU
+    layers.append(ExU(in_features=input_shape, out_features=num_units))
+
     for in_features, out_features in zip(hidden_sizes, hidden_sizes[1:]):
-      layers.append(layer(in_features=in_features, out_features=out_features))
+      layers.append(LinReLU(in_features=in_features, out_features=out_features))
 
     self.model = nn.Sequential(*layers)
 
   def forward(self, inputs) -> torch.Tensor:
     """Computes FeatureNN output with either evaluation or training mode."""
-    for layer in self.model:
-      inputs = F.dropout(layer(inputs), p=self.config.dropout)
-    # outputs = torch.squeeze(inputs, dim=1)
     outputs = inputs
+    for layer in self.model:
+      outputs = F.dropout(layer(outputs), p=self.config.dropout)
     return outputs
