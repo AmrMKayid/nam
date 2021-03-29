6360d9fd38665dba21c7d44cc2a56e43ad95ae60
AmrMKayid <amrmkayid@gmail.com>
2021-03-29 04:06:38 +0200

    Update `Engine` Class

--- nam/engine/engine.py
+++ nam/engine/engine.py
@@ -1,14 +1,6 @@
-import os
-
 import pytorch_lightning as pl
 import torch
-import torch.nn.functional as F
-from pytorch_lightning.metrics.functional import accuracy
 from torch import nn
-from torch.utils.data import DataLoader
-from torch.utils.data import random_split
-from torchvision import transforms
-from torchvision.datasets import MNIST
 
 from nam.engine.losses import penalized_loss
 from nam.types import Config
@@ -25,8 +17,8 @@ class Engine(pl.LightningModule):
     self.config = config
     self.model = model
 
-    self.loss = lambda inputs, targets: penalized_loss(self.config, self.model,
-                                                       inputs, targets)
+    self.criterion = lambda inputs, targets, fnns_out: penalized_loss(
+        self.config, inputs, targets, fnns_out)
 
     self.save_hyperparameters(vars(self.config))
 
@@ -34,46 +26,60 @@ class Engine(pl.LightningModule):
       self,
       inputs: torch.Tensor,
   ) -> torch.Tensor:
-    outputs = self.model(inputs)
-    return outputs
+    logits, fnns_out = self.model(inputs)
+    return logits, fnns_out
 
   def configure_optimizers(self):
-    optimizer = torch.optim.Adam(
-        self.parameters(),
-        lr=self.config.lr,
-        weight_decay=self.config.decay_rate,
-    )
-    return optimizer
+    optimizer = torch.optim.AdamW(self.parameters(),
+                                  lr=self.config.lr,
+                                  weight_decay=self.config.decay_rate)
+
+    scheduler = torch.optim.lr_scheduler.StepLR(optimizer,
+                                                gamma=0.995,
+                                                step_size=1)
+    return [optimizer], [scheduler]
 
   def training_step(self, batch, batch_idx):
     inputs, targets, *weights = batch
 
-    inputs = inputs.view(inputs.size(0), -1)
-    # predictions = self.model(inputs)
-    loss = self.loss(inputs, targets.squeeze())
+    logits, fnns_out = self.model(inputs)
+    loss = self.criterion(logits, targets, fnns_out)
 
-    self.log('train_loss', loss)
+    self.log('train_loss',
+             loss,
+             on_step=True,
+             on_epoch=True,
+             prog_bar=True,
+             logger=True)
 
-    return {'training_loss': loss}
+    return loss  #{'training_loss': loss}
 
   def validation_step(self, batch, batch_idx):
     inputs, targets, *weights = batch
 
-    inputs = inputs.view(inputs.size(0), -1)
-    # predictions = self.model(inputs)
-    loss = self.loss(inputs, targets.squeeze())
+    logits, fnns_out = self.model(inputs)
+    loss = self.criterion(logits, targets, fnns_out)
 
-    self.log('val_loss', loss)
+    self.log('val_loss',
+             loss,
+             on_step=True,
+             on_epoch=True,
+             prog_bar=True,
+             logger=True)
 
     return {'val_loss': loss}
 
   def test_step(self, batch, batch_idx):
     inputs, targets, *weights = batch
 
-    inputs = inputs.view(inputs.size(0), -1)
-    # predictions = self.model(inputs)
-    loss = self.loss(inputs, targets.squeeze())
+    logits, fnns_out = self.model(inputs)
+    loss = self.criterion(logits, targets, fnns_out)
 
-    self.log('test_loss', loss)
+    self.log('test_loss',
+             loss,
+             on_step=True,
+             on_epoch=True,
+             prog_bar=True,
+             logger=True)
 
     return {'test_loss': loss}
